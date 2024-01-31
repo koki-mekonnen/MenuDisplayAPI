03508015d6be9f27e099a906f8c349ac4bfc3f87
habjk <habibjmk@gmail.com>
2024-01-31 14:25:56 +0300

    allow CORS

--- cmd/server.go
+++ cmd/server.go
@@ -3,14 +3,20 @@ package main
 import (
 	"foodorderapi/internals/config"
 	"foodorderapi/routes"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 
 	e := echo.New()
 
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
 	config.Databaseinit()
 
 	routes.Foodorderroutes(e)
